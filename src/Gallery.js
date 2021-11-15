615eb19e79ec933e62702985ea088a7725068789
Debora Baddipudi <baddipudid@opentext.com>
2021-11-15 18:59:24 -0500

    added content for Home Page

---
+++ src/Gallery.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const Gallery = () => {
+    return (
+      <div>
+        <section id="hero-outer">
+        <div id="hero-image"></div>
+        <div id="cover-text">
+            <h2>Love Running - gallery Photos</h2>
+          
+        </div>
+    </section>
+    
+
+      </div>
+   
+    )
+  }
+  
+  export default Gallery;
--- src/Header.js
+++ src/Header.js
@@ -6,8 +6,24 @@ const Header = () => {
       <div>
         <center>
           <h1>My First React App - Love Running</h1>
+          <title>Love Running</title>
+  
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
+    <nav>
+         
+             
+                    <a href="signup.html">Sign Up</a>
+        
+                    <a href="gallery.html">Gallery</a>
+        
+                    <a class="active" href="index.html">Home</a>
+       
+       
+        </nav>
         </center>
       </div>
+   
     )
   }
   
